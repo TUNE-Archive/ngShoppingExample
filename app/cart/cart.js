2afe6cab3949d40633462e74957a943255fb754a
Jim Snavely <ludflu@gmail.com>
2014-10-21 22:16:32 -0400

    prune uneeded stuff and try to add some style

--- app/cart/cart.js
+++ app/cart/cart.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.cart', ['ngRoute'])
+angular.module('myApp.cart', ['ngRoute','ui.bootstrap'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/cart', {
